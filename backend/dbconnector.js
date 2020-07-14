91db379cf0fc5f3a1bdc79120d07faea5c653a4e
dio <fpx@pm.me>
2020-07-15 01:25:44 +0300

    get_position implemented, get_user altered

--- backend/dbconnector.js
+++ backend/dbconnector.js
@@ -1,19 +1,29 @@
 const con = require('./config/dbconfig')
 
 module.exports = {
-	// Возвращает id роли (role_id) и id пользователя (user_id) внутри объекта, если имя и пароль правильные, undefined в противном случае; err != NULL при ошибке связи с бд
+	// Возвращает имя, id роли (role_id) и id пользователя (user_id) внутри объекта, если имя и пароль правильные, undefined в противном случае; err != NULL при ошибке связи с бд
 	// role_id:
 	// 1 - hr
 	// 2 - руководитель
 	// 3 - сотрудник
 	get_user: (email, password, callback) => {
-		x = con.query(`select id as user_id, role_id from users where email = ? and password = ?`,
+		con.query(`select name, id as user_id, role_id from users where email = ? and password = ?`,
 		[email, password],
 		function (err, result) {
 			if (err)
 				return callback(err)
 			else
 				return callback(null, result[0])
+		});
+	},
+	get_position: (user_id, callback) => {
+		con.query(`select positions.name as position from plans left join positions on positions.id = plans.position_id where worker_id = ?`,
+		[user_id],
+		function (err, result) {
+			if (err)
+				return callback(err)
+			else
+				return callback(null, result)
 		});
 	}
 }
