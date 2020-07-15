2c1233a306f15977feb09fc8809d288a4ba7c399
dio <fpx@pm.me>
2020-07-15 20:38:54 +0300

    added POST request login handling

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-
 if (process.env.NODE_ENV !== 'production')
 	require('dotenv').config()
 
+const { get_user, get_position } = require('./dbconnector')
+const hashing = require('./hashing')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const dbconnect = require('./dbconnector')
 const port = process.env.EXPRESS_PORT || 9000
 
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors({
+	origin: "http://localhost:3000"
+}))
 
-app.get('/', (req, res) => {
-	res.send("Тут должно быть SPA")
-})
-
-app.post('/login', (req, res) => {
-	res.send({})
+app.post('/api/login', (req, res) => {
+	get_user(req.body.email, hashing(req.body.email, req.body.password), (err, result) => {
+		if (err)
+			res.status(500).send({error_message: "Невозможно подключиться к БД", error_flag: true})
+		res.status(200).send(result ? result : {empty: true})
+	})
 })
 
 app.listen(port, _ => {
