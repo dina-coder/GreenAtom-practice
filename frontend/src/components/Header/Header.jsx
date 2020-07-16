462ca0f0228dbd2afec8232c75607d4bd1389782
julry <julry6999@gmail.com>
2020-07-16 14:01:19 +0300

    Little changes to Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import style from "./Header.module.scss";
 import logoHeader from './../../img/logo_header.svg';
 
-const Header = () => {
+const Header = (props) => {
     return(
         <div className={style.header}> 
             <div className={style.headerContainer}>
                 <img src={logoHeader}></img>
                 <div className={style.userInfo}>
-                    <p>Фамилия Имя</p>
+                    <p>{props.name}</p>
                     <div className={style.userAdditionInfo}>
-                        <p>Сотрудник</p>
-                        <button className={style.headerButton}>Byiti</button>
+                        <p>{props.role}</p>
+                        <button className={style.headerButton}>Войти</button>
                     </div>
                 </div>
             </div>
