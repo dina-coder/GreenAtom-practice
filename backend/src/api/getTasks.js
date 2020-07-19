2e5dd82877e9af236481b38a64e0e7a29cae000c
dio <fpx@pm.me>
2020-07-19 11:38:00 +0300

    date format on db + getTasks router

---
+++ backend/src/api/getTasks.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const { db_error, generic_db_error, empty, get_tasks_path } = require('../misc/resources')
+const { get_tasks } = require('../misc/dbconnector')
+
+router.get(get_tasks_path, async (req, res) => {
+	try {
+		const result = await get_tasks(req.query.plan_id)
+		res.status(200).send(result[0] ? result : empty)
+	} catch (ex) {
+		console.log(ex)
+		res.status(500).send(db_error(generic_db_error))
+	}
+})
+
+module.exports = router
+
--- backend/src/app.js
+++ backend/src/app.js
@@ -12,10 +12,10 @@ const { generic_db_error, db_error,
 	empty, default_express_port, get_plans_worker_sql,
 	get_plans_super_sql, get_plans_hr_sql,
 	get_dict_grades_sql, get_dict_names_sql,
-	get_dict_steps_sql, get_dict_positions_sql, api_path
+	get_dict_steps_sql, get_dict_positions_sql, api_path, morgan_string
 } = require('./misc/resources')
 
-const { get_tasks, get_dict,
+const { get_dict,
 	insert_plan, insert_task,
 	update_plan, update_task,
 	delete_plan, delete_task,
@@ -26,21 +26,14 @@ const port = process.env.EXPRESS_PORT || default_express_port
 
 const loginRouter = require('./api/login')
 const getPlansWorkerRouter = require('./api/getPlansWorker')
+const getTasksRouter = require('./api/getTasks')
 
 app.use(express.json())
 app.use(cors({origin: frontend_origin}))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(morgan(morgan_string))
 app.use(api_path, loginRouter)
 app.use(api_path, getPlansWorkerRouter)
-
-app.get('/api/get_tasks', async (req, res) => {
-	try {
-		const result = await get_tasks(req.query.plan_id)
-		res.status(200).send(result[0] ? result : empty)
-	} catch (ex) {
-		res.status(500).send(db_error(generic_db_error))
-	}
-})
+app.use(api_path, getTasksRouter)
 
 app.get('/api/get_plans_super', async (req, res) => {
 	try {
