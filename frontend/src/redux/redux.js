c7d9e7aa7f9ce54c389e4a908d2431dac5e51159
unknown <egor0506070@mail.ru>
2020-07-25 19:52:50 +0300

    удаление обновления

--- frontend/src/redux/redux.js
+++ frontend/src/redux/redux.js
@@ -13,26 +13,7 @@ let reducers=combineReducers({
     DictReducer: DictReducer
 });
 
-const saveState = (state) => {
-    try {
-        const serialisedState = JSON.stringify(state);
-        window.localStorage.setItem('app_state', serialisedState);
-    } catch (err) {
-    }
-};
-const loadState = () => {
-    try {
-        const serialisedState = window.localStorage.getItem('app_state');
-        if (!serialisedState) return undefined;
-        return JSON.parse(serialisedState);
-    } catch (err) {
-        return undefined;
-    }
-};
-const oldState = loadState();
-export const store = createStore(reducers,oldState, applyMiddleware(thunkMiddleware));
-store.subscribe(() => {
-    saveState(store.getState());
-});
+
+export const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 
